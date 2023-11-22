a5c84db6ff915375d3433ca7a8a5854384cbef65
alexgitENI <alexgalaxy67@gmail.com>
2023-11-22 14:27:50 +0100

    test

--- test_lang.php
+++ test_lang.php
@@ -1,9 +1,5 @@
 <?php
 
-// Lire le contenu du fichier index.php
-$fileContent = file_get_contents('index.php');
-
-// Afficher le contenu du fichier
-var_dump($fileContent);
+var_dump($_SERVER);
 
 ?>
